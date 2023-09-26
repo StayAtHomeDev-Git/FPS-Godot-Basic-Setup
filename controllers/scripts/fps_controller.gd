0538b151d586acdf42801e29048e8b93c8a5d2cf
StayAtHomeDev <contact@stayathomedev.com>
2023-09-26 15:59:01 -0400

    Update fps controller script

--- controllers/scripts/fps_controller.gd
+++ controllers/scripts/fps_controller.gd
@@ -1,63 +1,65 @@
 extends CharacterBody3D
 
-
 @export var SPEED : float = 5.0
 @export var JUMP_VELOCITY : float = 4.5
-
 @export var MOUSE_SENSITIVITY : float = 0.5
 @export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
 @export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
-@export var CAMERA_CONTROLLER : Node3D
+@export var CAMERA_CONTROLLER : Camera3D
 
 var _mouse_input : bool = false
-var _mouse_rotation : Vector3
 var _rotation_input : float
 var _tilt_input : float
+var _mouse_rotation : Vector3
 var _player_rotation : Vector3
 var _camera_rotation : Vector3
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-# Check for initial input events
-func _input(event):
-	if event.is_action_pressed("exit"):
-		get_tree().quit()
-
-# Check for unhandled input events like mouse movement		
-func _unhandled_input(event):
+func _unhandled_input(event: InputEvent) -> void:
+	
 	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
 	if _mouse_input:
 		_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
 		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
 		
-# Updates the player controller camera based on the captured mouse movement data
+func _input(event):
+	
+	if event.is_action_pressed("exit"):
+		get_tree().quit()
+		
 func _update_camera(delta):
-
+	
+	# Rotates camera using euler rotation
 	_mouse_rotation.x += _tilt_input * delta
 	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
 	_mouse_rotation.y += _rotation_input * delta
 	
 	_player_rotation = Vector3(0.0,_mouse_rotation.y,0.0)
 	_camera_rotation = Vector3(_mouse_rotation.x,0.0,0.0)
-	
+
 	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
-	CAMERA_CONTROLLER.rotation.z = 0.0
-	
 	global_transform.basis = Basis.from_euler(_player_rotation)
 	
+	CAMERA_CONTROLLER.rotation.z = 0.0
+
 	_rotation_input = 0.0
 	_tilt_input = 0.0
-		
+	
 func _ready():
+
+	# Get mouse input
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
+	
+	# Update camera movement based on mouse movement
+	_update_camera(delta)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-		
-	_update_camera(delta)
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -66,7 +68,9 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
